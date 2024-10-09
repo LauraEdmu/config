f04920bbeeb5d693438df852354c1bdeacaf66ed
LauraEdmu <lauraedmu@pm.me>
2024-10-09 09:31:53 +0100

    Added rsyslog to deb packages, and added fedora version of QS

--- lauraQS_Fedora.sh
+++ lauraQS_Fedora.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# Note: might need to sed -i 's/\r$//' lauraQS_Ext.sh
-
 # Update and upgrade system
-sudo apt update && sudo apt upgrade -y
+sudo dnf update -y
 
 # Install necessary packages
-sudo apt install -y ufw rsyslog neovim curl git fail2ban upower fzf htop
+sudo dnf install -y ufw neovim curl git fail2ban upower fzf htop
 
-# Create a new user "laura" without a password and add to sudo group
+# Create a new user "laura" without a password and add to the wheel group (Fedora's sudo group)
 sudo useradd -m -s /bin/bash laura
-sudo usermod -aG sudo laura
+sudo usermod -aG wheel laura
 
 # Disable password login for the new user
 sudo passwd -d laura
@@ -32,41 +30,27 @@ sudo curl -s https://raw.githubusercontent.com/LauraEdmu/config/master/pub_keys/
 sudo chmod 600 /home/laura/.ssh/authorized_keys
 sudo chown -R laura:laura /home/laura/.ssh
 
-# Disable root login over SSH and disable password authentication
-#sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+# Disable password authentication for SSH
 sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-sudo systemctl restart ssh
+sudo systemctl restart sshd
 
 # Enable and configure UFW, allowing SSH (port 22) only
 sudo ufw allow proto tcp from any to any port 22
-#sudo ufw --force enable
 
 # Enable and start Fail2Ban for SSH protection
-sudo systemctl enable fail2ban
-sudo systemctl start fail2ban
+sudo systemctl enable --now fail2ban
 
 # Set the system timezone to Europe/London
 sudo timedatectl set-timezone Europe/London
 
+# Download .bashrc for root and user "laura"
 sudo curl -s https://raw.githubusercontent.com/LauraEdmu/config/master/.bashrc -o /root/.bashrc
 sudo curl -s https://raw.githubusercontent.com/LauraEdmu/config/master/.bashrc -o /home/laura/.bashrc
-
-# Install vim-plug for Neovim (for both laura and root)
-#curl -fLo /home/laura/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-#sudo curl -fLo /root/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-#sudo chown -R laura:laura /home/laura/.local
-
-# Set up Neovim for both users, download init.lua
-#mkdir -p /home/laura/.config/nvim
-#curl -s https://raw.githubusercontent.com/LauraEdmu/config/refs/heads/master/nvim/init.lua -o /home/laura/.config/nvim/init.lua
-#mkdir -p /root/.config/nvim
-#sudo curl -s https://raw.githubusercontent.com/LauraEdmu/config/refs/heads/master/nvim/init.lua -o /root/.config/nvim/init.lua
 
 # Ensure laura owns all files in her home directory
 sudo chown -R laura:laura /home/laura
 
-sudo source ~/.bashrc
+# Source the bashrc for immediate effect
+source ~/.bashrc
 
-echo "Setup complete! You may now want to Enable UFW (Already configured) A system reboot is recommended to apply all changes."
+echo "Setup complete! You may now want to Enable UFW (Already configured). A system reboot is recommended to apply all changes."
